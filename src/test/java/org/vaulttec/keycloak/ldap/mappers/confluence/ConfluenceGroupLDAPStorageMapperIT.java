481e1fabcc8eba19ba6a65bfbc4bc8005cc40c26
Torsten Juergeleit <torsten.juergeleit@gmail.com>
2024-03-20 00:10:02 +0100

    Adds additional test and comments; improves parametrized test

--- src/test/java/org/vaulttec/keycloak/ldap/mappers/confluence/ConfluenceGroupLDAPStorageMapperIT.java
+++ src/test/java/org/vaulttec/keycloak/ldap/mappers/confluence/ConfluenceGroupLDAPStorageMapperIT.java
@@ -1,15 +1,20 @@
 package org.vaulttec.keycloak.ldap.mappers.confluence;
 
-import dasniko.testcontainers.keycloak.KeycloakContainer;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.resource.RealmResource;
 import org.keycloak.admin.client.resource.UserResource;
 import org.keycloak.admin.client.resource.UsersResource;
+import org.keycloak.representations.idm.ComponentRepresentation;
 import org.keycloak.representations.idm.GroupRepresentation;
+import org.keycloak.representations.idm.SynchronizationResultRepresentation;
 import org.keycloak.representations.idm.UserRepresentation;
+import org.keycloak.storage.UserStorageProvider;
 
 import java.util.List;
 
@@ -17,8 +22,7 @@ import static dasniko.testcontainers.keycloak.ExtendableKeycloakContainer.MASTER
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ConfluenceGroupLDAPStorageMapperIT {
     public static final String REALM = "acme";
@@ -37,28 +41,55 @@ public class ConfluenceGroupLDAPStorageMapperIT {
     @ParameterizedTest
     @ValueSource(strings = {MASTER_REALM, REALM})
     public void testRealms(String realm) {
-        String accountServiceUrl = given().when().get(KEYCLOAK_ENVIRONMENT.getAuthServerUrl() + "/realms/" + realm)
-                .then().statusCode(200).body("realm", equalTo(realm))
-                .extract().path("account-service");
-
-        given().when().get(accountServiceUrl).then().statusCode(200);
+        // Retrieve JSON representation of realm definition and extract account service URL
+        String accountServiceUrl = given()
+                .when()
+                    .get(KEYCLOAK_ENVIRONMENT.getAuthServerUrl() + "/realms/" + realm)
+                .then()
+                    .statusCode(200).body("realm", equalTo(realm))
+                .extract()
+                    .path("account-service");
+        // Access account service
+        given()
+                .when()
+                    .get(accountServiceUrl)
+                .then()
+                    .statusCode(200);
     }
 
-    @ParameterizedTest
-    @ValueSource(strings = {"dooj1:johndoo", "dooj2:janedoo", "dooj3:jimdoo"})
-    public void testAccessingUsersAsAdmin(String usernameAndMailname) {
-        String[] splittedParameter = usernameAndMailname.split(":");
+    @ParameterizedTest(name="username {0} has email {1}@ns-mail8.com")
+    @CsvSource({"dooj1,johndoo", "dooj2,janedoo", "dooj3,jimdoo"})
+    public void testAccessingUsersAsAdmin(String username, String email) {
         Keycloak kcAdmin = KEYCLOAK_ENVIRONMENT.getAdminClient();
         UsersResource usersResource = kcAdmin.realm(REALM).users();
-        List<UserRepresentation> users = usersResource.search(splittedParameter[0]);
+        // Get user representation for given username
+        List<UserRepresentation> users = usersResource.search(username);
         assertThat(users, is(not(empty())));
-
         String userId = users.get(0).getId();
+        // Retrieve user details and check user's email address
         UserResource userResource = usersResource.get(userId);
-        assertThat(userResource.toRepresentation().getEmail(), is(splittedParameter[1] + "@ns-mail8.com"));
-
+        assertThat(userResource.toRepresentation().getEmail(), is(email + "@ns-mail8.com"));
+        // Retrieve user's groups and check group membership
         List<GroupRepresentation> groups = userResource.groups();
         assertEquals(2, groups.size());
         assertTrue(groups.stream().allMatch(group -> "Page 1.1".equals(group.getName()) || "Page 1.2".equals(group.getName())));
+    }
+
+    @Test
+    public void testFullSync() {
+        Keycloak kcAdmin = KEYCLOAK_ENVIRONMENT.getAdminClient();
+        RealmResource realm = kcAdmin.realm(REALM);
+        // Retrieve ID of LDAP user federation provider
+        List<ComponentRepresentation> storageProviders = realm.components().query(realm.toRepresentation().getId(), UserStorageProvider.class.getName());
+        assertThat(storageProviders, is(not(empty())));
+        String storageProviderId = storageProviders.get(0).getId();
+        // Trigger full sync of users and groups
+        SynchronizationResultRepresentation syncResult = realm.userStorage().syncUsers(storageProviderId, "triggerFullSync");
+        assertNotNull(syncResult);
+        assertEquals(102, syncResult.getAdded());
+        // Check groups hierarchy synced from Confluence
+        List<GroupRepresentation> groups = realm.groups().query("Page", true);
+        assertEquals(2, groups.size());
+        assertEquals(2, groups.get(0).getSubGroupCount());
     }
 }
